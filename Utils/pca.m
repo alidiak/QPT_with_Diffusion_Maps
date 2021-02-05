fddd2076fa0af959474eb8e96c9e095e41e670e1
Alex <38960233+alidiak@users.noreply.github.com>
2021-02-05 11:50:54 -0700

    Update pca.m

--- Utils/pca.m
+++ Utils/pca.m
@@ -1,24 +1,9 @@
 function [U, S] = pca(X)
-%PCA Run principal component analysis on the dataset X
-%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
-%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
-%
 
-% Useful values
 [m, n] = size(X);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: You should first compute the covariance matrix. Then, you
-%               should use the "svd" function to compute the eigenvectors
-%               and eigenvalues of the covariance matrix. 
-%
-% Note: When computing the covariance matrix, remember to divide by m (the
-%       number of examples).
-%
 
 covariancemat = (1/m)*((X).')*X;
 [U,S] = svd(covariancemat);
 
-% =========================================================================
 
 end
